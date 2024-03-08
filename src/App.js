e6ea3599627793ea3908d88a3325520d44f73318
maliha shahi <maliha.shahi.03@gmail.com>
2024-03-08 23:20:36 +0330

    modefi file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Index from './layouts/admin/Index';
+
+function App() {
+  return (
+    <div className="App">
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
