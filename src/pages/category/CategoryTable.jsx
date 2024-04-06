217176cc55bdfd2adbc7bdfae6f2f6fa07c665b0
maliha shahi <maliha.shahi.03@gmail.com>
2024-04-06 23:20:38 +0430

    add new category and remove involid code after geting response with axios intercepter

--- src/pages/category/CategoryTable.jsx
+++ src/pages/category/CategoryTable.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import PaginatedTable from "../../components/PaginatedTable";
 import { getCategoriesService } from "../../services/category";
 import { Alert } from "../../utils/alerts";
+import { convertDateToJalali } from "../../utils/convertDate";
 import Addcategory from "./AddCategory";
 import Actions from "./tableAdditons/Actions";
 import ShowInMenu from "./tableAdditons/ShowInMenu";
@@ -11,32 +12,33 @@ const Categorytable = () => {
   const params = useParams();
   const location = useLocation();
   const [data, setData] = useState([]);
+  const [forceRender, setForceRender] = useState(0);
   const handleGetCategories = async () => {
     try {
       const res = await getCategoriesService(params.categoryId);
       if (res.status === 200) {
         setData(res.data.data);
-      } else {
-        Alert("مشکل...!", res.data.message, "error");
       }
     } catch (error) {
-      Alert("مشکل...!", "مشکلی از سمت سرور رخ داده", "error");
+      console.log(error.message);
     }
   };
 
   useEffect(() => {
-    console.log(location);
     handleGetCategories();
-  }, [params]);
+  }, [params, forceRender]);
 
   const dataInfo = [
     { field: "id", title: "#" },
     { field: "title", title: "عنوان محصول" },
     { field: "parent_id", title: "والد" },
-    { field: "created_at", title: "تارخ" },
   ];
 
   const additionField = [
+    {
+      title: "تاریخ",
+      elements: (rowData) => convertDateToJalali(rowData.created_at),
+    },
     {
       title: "نمایش در منو",
       elements: (rowData) => <ShowInMenu rowData={rowData} />,
@@ -55,196 +57,21 @@ const Categorytable = () => {
 
   return (
     <>
-      {location.state ? (
-        <h5 className="text-center">
-          <span>زیرگروه: </span>
-          <span className="text-info">{location.state.parentData.title}</span>
-        </h5>
-      ) : null}
-      <PaginatedTable
-        data={data}
-        dataInfo={dataInfo}
-        additionField={additionField}
-        numOfPAge={8}
-        searchParams={searchParams}
-      >
-        <Addcategory />
-      </PaginatedTable>
+      <Outlet />
+      {data.length ? (
+        <PaginatedTable
+          data={data}
+          dataInfo={dataInfo}
+          additionField={additionField}
+          numOfPAge={8}
+          searchParams={searchParams}
+        >
+          <Addcategory setForceRender={setForceRender}/>
+        </PaginatedTable>
+      ) : (
+        <h5 className="text-center my-5 text-danger">هیچ دسته بندی یافت نشد</h5>
+      )}
     </>
-    // <>
-    //   <table className="table table-responsive text-center table-hover table-bordered">
-    //     <thead className="table-secondary">
-    //       <tr>
-    //         <th>#</th>
-    //         <th>عنوان</th>
-    //         <th>وضعیت</th>
-    //         <th>عملیات</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       <tr>
-    //         <td>1</td>
-    //         <td>دسته شماره فلان</td>
-    //         <td>فعال</td>
-    //         <td>
-    //           <i
-    //             className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip"
-    //             title="زیرمجموعه"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip"
-    //             title="ویرایش دسته"
-    //             data-bs-toggle="modal"
-    //             data-bs-placement="top"
-    //             data-bs-target="#add_product_category_modal"
-    //           ></i>
-    //           <i
-    //             className="fas fa-plus text-success mx-1 hoverable_text pointer has_tooltip"
-    //             title="افزودن ویژگی"
-    //             data-bs-toggle="modal"
-    //             data-bs-target="#add_product_category_attr_modal"
-    //           ></i>
-    //           <i
-    //             className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip"
-    //             title="حذف دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //         </td>
-    //       </tr>
-    //       <tr>
-    //         <td>1</td>
-    //         <td>دسته شماره فلان</td>
-    //         <td>فعال</td>
-    //         <td>
-    //           <i
-    //             className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip"
-    //             title="زیرمجموعه"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip"
-    //             title="ویرایش دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-plus text-success mx-1 hoverable_text pointer has_tooltip"
-    //             title="افزودن ویژگی"
-    //             data-bs-placement="top"
-    //             data-bs-toggle="modal"
-    //             data-bs-target="#add_product_category_attr_modal"
-    //           ></i>
-    //           <i
-    //             className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip"
-    //             title="حذف دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //         </td>
-    //       </tr>
-    //       <tr>
-    //         <td>1</td>
-    //         <td>دسته شماره فلان</td>
-    //         <td>فعال</td>
-    //         <td>
-    //           <i
-    //             className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip"
-    //             title="زیرمجموعه"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip"
-    //             title="ویرایش دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-plus text-success mx-1 hoverable_text pointer has_tooltip"
-    //             title="افزودن ویژگی"
-    //             data-bs-placement="top"
-    //             data-bs-toggle="modal"
-    //             data-bs-target="#add_product_category_attr_modal"
-    //           ></i>
-    //           <i
-    //             className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip"
-    //             title="حذف دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //         </td>
-    //       </tr>
-    //       <tr>
-    //         <td>1</td>
-    //         <td>دسته شماره فلان</td>
-    //         <td>فعال</td>
-    //         <td>
-    //           <i
-    //             className="fas fa-project-diagram text-info mx-1 hoverable_text pointer has_tooltip"
-    //             title="زیرمجموعه"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip"
-    //             title="ویرایش دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //           <i
-    //             className="fas fa-plus text-success mx-1 hoverable_text pointer has_tooltip"
-    //             title="افزودن ویژگی"
-    //             data-bs-placement="top"
-    //             data-bs-toggle="modal"
-    //             data-bs-target="#add_product_category_attr_modal"
-    //           ></i>
-    //           <i
-    //             className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip"
-    //             title="حذف دسته"
-    //             data-bs-toggle="tooltip"
-    //             data-bs-placement="top"
-    //           ></i>
-    //         </td>
-    //       </tr>
-    //     </tbody>
-    //   </table>
-    //   <nav
-    //     aria-label="Page navigation example"
-    //     className="d-flex justify-content-center"
-    //   >
-    //     <ul className="pagination dir_ltr">
-    //       <li className="page-item">
-    //         <a className="page-link" href="#" aria-label="Previous">
-    //           <span aria-hidden="true">&raquo;</span>
-    //         </a>
-    //       </li>
-    //       <li className="page-item">
-    //         <a className="page-link" href="#">
-    //           1
-    //         </a>
-    //       </li>
-    //       <li className="page-item">
-    //         <a className="page-link" href="#">
-    //           2
-    //         </a>
-    //       </li>
-    //       <li className="page-item">
-    //         <a className="page-link" href="#">
-    //           3
-    //         </a>
-    //       </li>
-    //       <li className="page-item">
-    //         <a className="page-link" href="#" aria-label="Next">
-    //           <span aria-hidden="true">&laquo;</span>
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    // </>
   );
 };
 
