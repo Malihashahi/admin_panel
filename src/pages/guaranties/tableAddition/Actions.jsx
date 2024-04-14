728d486ca27100810568ccca8a9ca6b6c5c20b2b
maliha shahi <maliha.shahi.03@gmail.com>
2024-04-14 22:51:01 +0430

    work on management of guaranties page like delete edit ...

--- src/pages/guaranties/tableAddition/Actions.jsx
+++ src/pages/guaranties/tableAddition/Actions.jsx
@@ -1,23 +1,23 @@
 
 import React from "react";
-const Actions = ({ rowData, setBrandToEdit, handleDeleteBrand}) => {
+const Actions = ({ rowData, setGuaranteeToEdit, handleDeleteGuarantee}) => {
   return (
     <>
       <i
         className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip"
-        title="ویرایش برند"
+        title="ویرایش گارانتی"
         data-bs-placement="top"
         data-bs-toggle="modal"
-        data-bs-target="#add_brand_modal"
-        onClick={()=>setBrandToEdit(rowData)}
+        data-bs-target="#add_guarantee_modal"
+        onClick={()=>setGuaranteeToEdit(rowData)}
       ></i>
 
       <i
         className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip"
-        title="حذف برند"
+        title="حذف گارانتی"
         data-bs-toggle="tooltip"
         data-bs-placement="top"
-        onClick={()=>{handleDeleteBrand(rowData)}}
+        onClick={()=>{handleDeleteGuarantee(rowData)}}
       ></i>
     </>
   );
