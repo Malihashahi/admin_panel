7bf114bd5e8def4eb4f5b03274ffec64e6bfaecb
maliha shahi <maliha.shahi.03@gmail.com>
2024-04-15 22:43:26 +0430

    work on management of colors component and modiy product .

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,7 +1,5 @@
 
 import React from "react";
-import AddProduct from "./AddProduct";
-import SetAttribute from "./SetAttribute";
 import TableProduct from "./TableProduct";
 
 const Product = () => {
@@ -11,23 +9,7 @@ const Product = () => {
       className="manage_product_section main_section"
     >
       <h4 className="text-center my-3">مدیریت محصولات</h4>
-      <div className="row justify-content-between">
-        <div className="col-10 col-md-6 col-lg-4">
-          <div className="input-group mb-3 dir_ltr">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="قسمتی از عنوان را وارد کنید"
-            />
-            <span className="input-group-text">جستجو</span>
-          </div>
-        </div>
-        <div className="col-2 col-md-6 col-lg-4 d-flex flex-column align-items-end">
-          <AddProduct />
-        </div>
-      </div>
       <TableProduct />
-      <SetAttribute />
     </div>
   );
 };
